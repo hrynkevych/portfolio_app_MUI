b8a9707c1f3e3bfe92a1b72bc0715fd6f25aec95
Mykhailo Hrynkevych <mgrynkevych@outlook.com>
2024-03-28 22:03:09 +0100

    header

---
+++ portfolio_app/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { MenuAppBar } from "./components/app-bar/AppBar";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MenuAppBar />
+      </header>
+      <body></body>
+    </div>
+  );
+}
+
+export default App;
